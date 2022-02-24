d4547e980f7707eddcc0c861b52fed427cbc9789
Roman Popelka <rpopelka@redhat.com>
2022-02-24 10:03:14 +0100

    Remove create_keypair & create_server methods
    As tempest.scenario.manager was announced stable interface in Tempest
    27.0.0[1] it can be now reused in plugins.
    
    Replaced methods:
        * create_keypair
        * create_server
    
    Etherpad concerning this effort:
    https://etherpad.opendev.org/p/tempest-scenario-manager-cleanup
    
    [1] https://docs.openstack.org/releasenotes/tempest/v27.0.0.html#release-notes-27-0-0
    
    Change-Id: I31bd218a600f5c60002ceef01d8f9345e7b95f85

--- cyborg_tempest_plugin/tests/scenario/manager.py
+++ cyborg_tempest_plugin/tests/scenario/manager.py
@@ -19,9 +19,7 @@ from cyborg_tempest_plugin.services.cyborg_rest_client import get_auth_provider
 
 from oslo_log import log
 
-from tempest.common import compute
 from tempest.common import credentials_factory as common_creds
-from tempest.common import waiters
 from tempest import config
 from tempest.lib.common.utils import data_utils
 from tempest.lib.common.utils import test_utils
@@ -34,7 +32,7 @@ CONF = config.CONF
 LOG = log.getLogger(__name__)
 
 
-class ScenarioTest(tempest.test.BaseTestCase):
+class ScenarioTest(tempest.scenario.manager.ScenarioTest):
     """Base class for scenario tests. Uses tempest own clients. """
 
     credentials = ['primary', 'admin']
@@ -87,15 +85,6 @@ class ScenarioTest(tempest.test.BaseTestCase):
     # The create_[resource] functions only return body and discard the
     # resp part which is not used in scenario tests
 
-    def create_keypair(self, client=None):
-        if not client:
-            client = self.keypairs_client
-        name = data_utils.rand_name(self.__class__.__name__)
-        # We don't need to create a keypair by pubkey in scenario
-        body = client.create_keypair(name=name)
-        self.addCleanup(client.delete_keypair, name)
-        return body['keypair']
-
     def update_flavor_extra_specs(self, specs, flavor):
         set_body = self.admin_flavors_client.set_flavor_extra_spec(
             flavor['id'], **specs)['extra_specs']
@@ -144,111 +133,6 @@ class ScenarioTest(tempest.test.BaseTestCase):
         specs = {"accel:device_profile": dp_name}
         self.update_flavor_extra_specs(specs, flavor)
         return flavor["id"]
-
-    def create_server(self, name=None, image_id=None, flavor=None,
-                      validatable=False, wait_until='ACTIVE',
-                      clients=None, **kwargs):
-
-        """Wrapper utility that returns a test server.
-
-        This wrapper utility calls the common create test server and
-        returns a test server. The purpose of this wrapper is to minimize
-        the impact on the code of the tests already using this
-        function.
-
-        """
-
-        # NOTE(jlanoux): As a first step, ssh checks in the scenario
-        # tests need to be run regardless of the run_validation and
-        # validatable parameters and thus until the ssh validation job
-        # becomes voting in CI. The test resources management and IP
-        # association are taken care of in the scenario tests.
-        # Therefore, the validatable parameter is set to false in all
-        # those tests. In this way create_server just return a standard
-        # server and the scenario tests always perform ssh checks.
-
-        # Needed for the cross_tenant_traffic test:
-        if clients is None:
-            clients = self.os_primary
-
-        if name is None:
-            name = data_utils.rand_name(self.__class__.__name__ + "-server")
-
-        vnic_type = CONF.network.port_vnic_type
-        profile = CONF.network.port_profile
-
-        # If vnic_type or profile are configured create port for
-        # every network
-        if vnic_type or profile:
-            ports = []
-            create_port_body = {}
-
-            if vnic_type:
-                create_port_body['binding:vnic_type'] = vnic_type
-
-            if profile:
-                create_port_body['binding:profile'] = profile
-
-            if kwargs:
-                # Convert security group names to security group ids
-                # to pass to create_port
-                if 'security_groups' in kwargs:
-                    security_groups = \
-                        clients.security_groups_client.list_security_groups(
-                        ).get('security_groups')
-                    sec_dict = dict([(s['name'], s['id'])
-                                    for s in security_groups])
-
-                    sec_groups_names = [s['name'] for s in kwargs.pop(
-                        'security_groups')]
-                    security_groups_ids = [sec_dict[s]
-                                           for s in sec_groups_names]
-
-                    if security_groups_ids:
-                        create_port_body[
-                            'security_groups'] = security_groups_ids
-                networks = kwargs.pop('networks', [])
-            else:
-                networks = []
-
-            # If there are no networks passed to us we look up
-            # for the project's private networks and create a port.
-            # The same behaviour as we would expect when passing
-            # the call to the clients with no networks
-            if not networks:
-                networks = clients.networks_client.list_networks(
-                    **{'router:external': False, 'fields': 'id'})['networks']
-
-            # It's net['uuid'] if networks come from kwargs
-            # and net['id'] if they come from
-            # clients.networks_client.list_networks
-            for net in networks:
-                net_id = net.get('uuid', net.get('id'))
-                if 'port' not in net:
-                    port = self.create_port(network_id=net_id,
-                                            client=clients.ports_client,
-                                            **create_port_body)
-                    ports.append({'port': port['id']})
-                else:
-                    ports.append({'port': net['port']})
-            if ports:
-                kwargs['networks'] = ports
-            self.ports = ports
-
-        tenant_network = self.get_tenant_network()
-        body, _ = compute.create_test_server(
-            clients,
-            tenant_network=tenant_network,
-            wait_until=wait_until,
-            name=name, flavor=flavor,
-            image_id=image_id, **kwargs)
-
-        self.addCleanup(waiters.wait_for_server_termination,
-                        clients.servers_client, body['id'])
-        self.addCleanup(test_utils.call_and_ignore_notfound_exc,
-                        clients.servers_client.delete_server, body['id'])
-        server = clients.servers_client.show_server(body['id'])['server']
-        return server
 
     def _create_loginable_secgroup_rule(self, secgroup_id=None):
         _client = self.compute_security_groups_client
